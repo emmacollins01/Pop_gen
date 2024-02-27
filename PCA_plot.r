3041bac489244e7717453d14797ce00905fe0611
emmacollins01 <emmacollins01@hotmail.co.uk>
2024-02-27 16:10:30 +0000

    Update with PR subset

--- PCA_plot.r
+++ PCA_plot.r
@@ -98,12 +98,19 @@ dev.off()
 ###############  Creating the distance matrix  ###############
 ##############################################################
 
+PR_meta <- read.csv("/mnt/storage12/emma/PR_combine/all_samples_PR_resistance.csv")
+subset_SRR <- unique(PR_meta$ID) 
+
+subset <- mat_bin_samples[,colnames(mat_bin_samples) %in% subset_SRR]
+
 dist_dat<-dist(t(mat_bin_samples), method = "manhattan")
+dist_dat_PR<-dist(t(subset), method = "manhattan")
 #dist_dat<-dist(t(snp_imputed_freq_t), method = "manhattan")
 #write.csv(dist_dat, "all_aedes_norm_ filt_miss0.5_mac3_minQ30.vcf.gz.recode.dist.mat")
 
 #Formatting for a PCA
 cmd_dat<-cmdscale(dist_dat, k = 10, eig = TRUE, x.ret = TRUE)
+cmd_dat_PR<-cmdscale(dist_dat_PR, k = 10, eig = TRUE, x.ret = TRUE)
 #write.csv(cmd_dat, "all_aedes_norm_filt_miss0.5_mac3_minQ30.vcf.gz.recode.dist.mat.pca")
 
 
@@ -115,31 +122,54 @@ calc_variance_explained <- function(pc_points) {
 }
 
 vars <- calc_variance_explained(cmd_dat)
+vars_PR <- calc_variance_explained(cmd_dat_PR)
 PC1_lab <- vars["PC1"]
 PC2_lab <- vars["PC2"]
 PC3_lab <- vars["PC3"]
 
+PC1_lab <- vars_PR["PC1"]
+PC2_lab <- vars_PR["PC2"]
+PC3_lab <- vars_PR["PC3"]
+
 ##############################################################
 ##########################  PCA plot  ########################
 ##############################################################
+
+
 df <- as.data.frame(cmd_dat$points, stringsAsFactors = F)
 colnames(df) <- gsub("V", "PC", colnames(df))
 df$Country <- metadata$Country
 df$Region <- metadata$Region
+df$Subregion <- metadata$Subregion
+
+# filtered version
+library(stringr)
+df_PR <- as.data.frame(cmd_dat_PR$points, stringsAsFactors = F)
+colnames(df_PR) <- gsub("V", "PC", colnames(df_PR))
+df_PR$Country <- PR_meta$Country
+df_PR$Region <- PR_meta$Region
+df_PR$Subregion <- PR_meta$Subregion
+for (i in 1:nrow(df_PR)){
+    df_PR$Subregion_simple[i] <- unlist(strsplit(df_PR$Subregion[i], ":"))[[2]]
+}
+
+
 
 #DEFINE COLOUR TO USE
 #col_ramp <- brewer.pal(8, "Pastel1")
 col_ramp <- c("#40476D", "#678D58","#EE964B", "#F95738",  "#A6C48A",  "#51A3A3", "#7D387D", "#bc80bd")
 #col_ramp <- c("#db7376", "#52a884", "#bc80bd", "#465c7a", "#ffaa5c",  "#A6C48A",  "#51A3A3", "#7D387D")
 colour_by <- "Country"
+colour_by <- "Subregion_simple"
 
 
+## change data input if using filtered version
 ####### PC1 vs PC2 #######
 plot <- ggplot() + 
-    geom_point(data = df, aes(x = PC1, y = PC2, colour = !!sym(colour_by)), size = 4) +
+    geom_point(data = df_PR, aes(x = PC1, y = PC2, colour = !!sym(colour_by)), size = 5) +
     xlab(paste0("PC1 ", PC1_lab, "%")) +
     ylab(paste0("PC2 ", PC2_lab, "%")) +
-    scale_colour_manual(values = col_ramp) +
+    scale_colour_manual(values = col_ramp, name = "Subregion") +
     theme_classic() +
     theme(legend.position = "bottom",
             axis.text = element_text(size = 24),
@@ -150,10 +180,12 @@ plot <- ggplot() +
 plot
 tiff("/mnt/storage12/emma/PR_combine/PCA_plots/PCA_all_samples_PC1_PC2_lmiss.tiff",height=1200,width=1200)
 dev.off()
+tiff("/mnt/storage12/emma/PR_combine/PCA_plots/PCA_PR_samples_PC1_PC2_lmiss.tiff",height=1200,width=1200)
+
 
 ####### PC1 vs PC3 #######
 plot <- ggplot() + 
-    geom_point(data = df, aes(x = PC1, y = PC3, colour = !!sym(colour_by)), size = 4) +
+    geom_point(data = df_PR, aes(x = PC1, y = PC3, colour = !!sym(colour_by)), size = 4) +
     xlab(paste0("PC1 ", PC1_lab, "%")) +
     ylab(paste0("PC3 ", PC3_lab, "%")) +
     scale_colour_manual(values = col_ramp) +
@@ -267,6 +299,10 @@ require("ape")
 
 tree_dat<-nj(dist_dat)
 write.tree(tree_dat, file="all_samples_lmiss.tree")
+
+
+tree_dat<-nj(dist_dat_PR)
+write.tree(tree_dat, file="PR_lmiss.tree")
 
 ## open in iTOL
 
